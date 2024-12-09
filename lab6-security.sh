7ad826ab7beef74d1f1c5211062f858aa99b209b
lesarmiento37 <lesarmiento@unal.edu.co>
2024-12-09 06:15:01 -0500

    Adding lab 6 security

---
+++ lab6-security.sh
@@ -0,0 +1,7 @@
+#Create Topic
+./bin/kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+# Create producer
+./bin/kafka-console-producer.sh --topic test-topic --bootstrap-server localhost:9092
+# Create consumer
+./bin/kafka-console-consumer.sh --topic test-topic --bootstrap-server localhost:9092 --from-beginning
+
